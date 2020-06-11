356ca8ac24ceeca4a0fd04dc3e032c3c5bc317b3
Murilo Cunha <murilo.sant@hotmail.com>
2020-06-11 14:02:19 -0300

    feat(mobile): added navigation

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+import styles from "./styles";
+
+const Points: React.FC = () => {
+  return <View />;
+};
+
+export default Points;
